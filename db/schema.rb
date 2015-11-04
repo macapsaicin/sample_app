5f21279f49e3caf1dd91ce6d3fbaa4ff6989ea9f
macapsaicin <stake5998@gmail.com>
2015-11-04 06:13:58 +0000

    Make a Micropost model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151103080700) do
+ActiveRecord::Schema.define(version: 20151104052103) do
+
+  create_table "microposts", force: true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",    null: false
